40a8c079bc72095baa3f28994f37be19863cc03e
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2025-12-31 17:11:57 +0530

    Refactor CORS configuration to use allowed origins array for better control

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,23 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://earnest-toffee-6a5983.netlify.app", // your Netlify URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // Postman or server requests
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
