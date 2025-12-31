e7f06c57d41f3afd4e034de0d613d7ec91998871
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2025-12-31 15:26:56 +0530

    Changes Host part

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
