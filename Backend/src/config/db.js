08c1566688f395fa6b8d897fdc574760653c31c3
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2025-12-31 16:17:35 +0530

    Fix database connection URI and set default port for server

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connect_DB = async () => {
   try {
     const db_instance = await mongoose.connect(
-      `${process.env.MONGO_DB_STR}/${DB_NAME}`
+      `${process.env.MONGO_URI}/${DB_NAME}`
     );
     console.log(
       `Database connected successfully ${db_instance.connection.name} `
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,6 +7,6 @@ import connect_DB from "./config/db.js";
 
 connect_DB();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Sever started SuccessFully at Port", process.env.PORT);
 });
